0402fe32c1ac4b2410e7d742c4ffaa18e3cfe56a
Saleh Hadi Aburah <sasa97977s@hotmail.com>
2025-03-11 17:14:55 +0300

    update the video url

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactPlayer from "react-player";
-
-import introVideo from "../videos/introVideo1.mp4";
 import { useMediaQuery, useTheme } from "@mui/material";
 
 export default function VideoPlayer() {
@@ -17,10 +15,10 @@ export default function VideoPlayer() {
       }}
     >
       <ReactPlayer
-        url={introVideo} // Pass the path to the local video here
+        url={"https://youtu.be/Vm8wjI3g2gU?si=ByQFYK0Eq7sf7GNQ"} // Pass the path to the local video here
         controls={true} // Adds play/pause, volume, etc.
-        width="100%" // Make the player responsive
-        height="auto" // Adjust height automatically
+        width={matches ? "550px" : "300px"} // Make the player responsive
+        height={matches ? "550px" : "300px"} // Make the player responsive
       />
     </div>
   );
